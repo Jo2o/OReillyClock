90f2a460258d626f026c422791802f64a9f98211
Jozef Mikuš <Jozef.Mikus@ocean.ibm.com>
2022-01-04 23:22:47 +0100

    -- seconds added

--- src/oreillyclock/views/ClockView.java
+++ src/oreillyclock/views/ClockView.java
@@ -1,31 +1,19 @@
 package oreillyclock.views;
 
 
+import java.time.LocalTime;
 import javax.inject.Inject;
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.IMenuListener;
-import org.eclipse.jface.action.IMenuManager;
-import org.eclipse.jface.action.IToolBarManager;
-import org.eclipse.jface.action.MenuManager;
-import org.eclipse.jface.action.Separator;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.DoubleClickEvent;
-import org.eclipse.jface.viewers.IDoubleClickListener;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchActionConstants;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
 
@@ -53,10 +41,30 @@ public class ClockView extends ViewPart {
     IWorkbench workbench;
 
     private TableViewer viewer;
-    private Action action1;
-    private Action action2;
-    private Action doubleClickAction;
 
+    @Override
+    public void createPartControl(Composite parent) {
+        final Canvas clock = new Canvas(parent, SWT.NONE);
+        clock.addPaintListener(this::drawClock);
+    }
+
+    private void drawClock(PaintEvent paintEvent) {
+        paintEvent.gc.drawArc(paintEvent.x, paintEvent.y, paintEvent.width - 1, paintEvent.height - 1, 0, 360);
+        drawSecondHand(paintEvent);
+    }
+
+    private void drawSecondHand(PaintEvent paintEvent) {
+        int seconds = LocalTime.now().getSecond();
+        int arc = (15 - seconds) * 6 % 360;
+        Color blue = paintEvent.display.getSystemColor(SWT.COLOR_BLUE);
+        paintEvent.gc.setBackground(blue);
+        paintEvent.gc.fillArc(paintEvent.x, paintEvent.y, paintEvent.width - 1, paintEvent.height - 1, arc - 1, 2);
+    }
+
+    @Override
+    public void setFocus() {
+        viewer.getControl().setFocus();
+    }
 
     class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
         @Override
@@ -73,123 +81,6 @@ public class ClockView extends ViewPart {
         public Image getImage(Object obj) {
             return workbench.getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
         }
-    }
-
-    @Override
-    public void createPartControl(Composite parent) {
-
-        final Canvas clock = new Canvas(parent, SWT.NONE);
-        clock.addPaintListener(this::drawClock);
-
-        // viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-        //
-        // viewer.setContentProvider(ArrayContentProvider.getInstance());
-        // viewer.setInput(new String[] {"One", "Two", "Three"});
-        // viewer.setLabelProvider(new ViewLabelProvider());
-        //
-        // // Create the help context id for the viewer's control
-        // workbench.getHelpSystem().setHelp(viewer.getControl(), "OReillyClock.viewer");
-        // getSite().setSelectionProvider(viewer);
-        // makeActions();
-        // hookContextMenu();
-        // hookDoubleClickAction();
-        // contributeToActionBars();
-    }
-
-    private void drawClock(PaintEvent paintEvent) {
-        paintEvent.gc.drawArc(paintEvent.x, paintEvent.y, paintEvent.width - 1,
-                paintEvent.height - 1, 0, 360);
-    }
-
-    private void hookContextMenu() {
-        MenuManager menuMgr = new MenuManager("#PopupMenu");
-        menuMgr.setRemoveAllWhenShown(true);
-        menuMgr.addMenuListener(new IMenuListener() {
-            @Override
-            public void menuAboutToShow(IMenuManager manager) {
-                ClockView.this.fillContextMenu(manager);
-            }
-        });
-        Menu menu = menuMgr.createContextMenu(viewer.getControl());
-        viewer.getControl().setMenu(menu);
-        getSite().registerContextMenu(menuMgr, viewer);
-    }
-
-    private void contributeToActionBars() {
-        IActionBars bars = getViewSite().getActionBars();
-        fillLocalPullDown(bars.getMenuManager());
-        fillLocalToolBar(bars.getToolBarManager());
-    }
-
-    private void fillLocalPullDown(IMenuManager manager) {
-        manager.add(action1);
-        manager.add(new Separator());
-        manager.add(action2);
-    }
-
-    private void fillContextMenu(IMenuManager manager) {
-        manager.add(action1);
-        manager.add(action2);
-        // Other plug-ins can contribute there actions here
-        manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
-    }
-
-    private void fillLocalToolBar(IToolBarManager manager) {
-        manager.add(action1);
-        manager.add(action2);
-    }
-
-    private void makeActions() {
-        action1 = new Action() {
-            @Override
-            public void run() {
-                showMessage("Action 1 executed");
-            }
-        };
-        action1.setText("Action 1");
-        action1.setToolTipText("Action 1 tooltip");
-        action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages()
-                .getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-
-        action2 = new Action() {
-            @Override
-            public void run() {
-                showMessage("Action 2 executed");
-            }
-        };
-        action2.setText("Action 2");
-        action2.setToolTipText("Action 2 tooltip");
-        action2.setImageDescriptor(
-                workbench.getSharedImages().getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-        doubleClickAction = new Action() {
-            @Override
-            public void run() {
-                IStructuredSelection selection = viewer.getStructuredSelection();
-                Object obj = selection.getFirstElement();
-                showMessage("Double-click detected on " + obj.toString());
-            }
-        };
-    }
-
-    private void hookDoubleClickAction() {
-        viewer.addDoubleClickListener(new IDoubleClickListener() {
-            @Override
-            public void doubleClick(DoubleClickEvent event) {
-                doubleClickAction.run();
-            }
-        });
-    }
-
-    private void showMessage(String message) {
-        MessageDialog.openInformation(
-                viewer.getControl().getShell(),
-                "ClockView",
-                message);
-    }
-
-    @Override
-    public void setFocus() {
-        viewer.getControl().setFocus();
     }
 
 }
