df3533143cc5e8a68c4e149843c46a521bbede97
Jozef Mikuš <Jozef.Mikus@ocean.ibm.com>
2022-01-05 16:21:47 +0100

    -- imageregistry

--- src/oreillyclock/forjfacetree/TimeZoneLabelProvider.java
+++ src/oreillyclock/forjfacetree/TimeZoneLabelProvider.java
@@ -2,9 +2,20 @@ package oreillyclock.forjfacetree;
 
 import java.time.ZoneId;
 import java.util.Map;
+import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.ISharedImages;
 
 public class TimeZoneLabelProvider extends LabelProvider {
+
+    private final ISharedImages images;
+    private final ImageRegistry imageRegistry;
+
+    public TimeZoneLabelProvider(ISharedImages images, ImageRegistry imageRegistry) {
+        this.images = images;
+        this.imageRegistry = imageRegistry;
+    }
 
     @Override
     public String getText(Object element) {
@@ -21,5 +32,17 @@ public class TimeZoneLabelProvider extends LabelProvider {
         } else {
             return "Unknown type: " + element.getClass();
         }
+    }
+
+    @Override
+    public Image getImage(Object element) {
+        if (element instanceof Map.Entry) {
+            return images.getImage(ISharedImages.IMG_OBJ_FOLDER);
+
+        } else if (element instanceof ZoneId) {
+            return imageRegistry.get("sample");
+        }
+
+        return super.getImage(element);
     }
 }
--- src/oreillyclock/views/TimeZoneTreeView.java
+++ src/oreillyclock/views/TimeZoneTreeView.java
@@ -1,22 +1,38 @@
 package oreillyclock.views;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.resource.LocalResourceManager;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.ISharedImages;
 import oreillyclock.TimeZoneComparator;
 import oreillyclock.forjfacetree.TimeZoneContentProvider;
 import oreillyclock.forjfacetree.TimeZoneLabelProvider;
 
 public class TimeZoneTreeView {
 
+    @Inject
+    private ISharedImages images;
+
     private TreeViewer treeViewer;
 
     @PostConstruct
     public void create(Composite parent) {
+
+        var resourceManager = JFaceResources.getResources();
+        var localResourceManager = new LocalResourceManager(resourceManager, parent);
+        var imageRegistry = new ImageRegistry(localResourceManager);
+
+        imageRegistry.put("sample", ImageDescriptor.createFromURL(getClass().getResource("/icons/sample.png")));
+
         treeViewer = new TreeViewer(parent, SWT.H_SCROLL | SWT.V_SCROLL | SWT.MULTI);
-        treeViewer.setLabelProvider(new TimeZoneLabelProvider());
+        treeViewer.setLabelProvider(new TimeZoneLabelProvider(images, imageRegistry));
         treeViewer.setContentProvider(new TimeZoneContentProvider());
         treeViewer.setInput(new Object[] {TimeZoneComparator.getTimeZones()});
     }
