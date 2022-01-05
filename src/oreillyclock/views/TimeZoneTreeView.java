0e5bd53a5231516b676c8fabce5cbdbbaeb23970
Jozef Mikuš <Jozef.Mikus@ocean.ibm.com>
2022-01-05 15:52:04 +0100

    -- tree view

--- src/oreillyclock/views/TimeZoneTreeView.java
+++ src/oreillyclock/views/TimeZoneTreeView.java
@@ -1,9 +1,13 @@
 package oreillyclock.views;
 
 import javax.annotation.PostConstruct;
+import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import oreillyclock.TimeZoneComparator;
+import oreillyclock.forjfacetree.TimeZoneContentProvider;
+import oreillyclock.forjfacetree.TimeZoneLabelProvider;
 
 public class TimeZoneTreeView {
 
@@ -12,6 +16,13 @@ public class TimeZoneTreeView {
     @PostConstruct
     public void create(Composite parent) {
         treeViewer = new TreeViewer(parent, SWT.H_SCROLL | SWT.V_SCROLL | SWT.MULTI);
+        treeViewer.setLabelProvider(new TimeZoneLabelProvider());
+        treeViewer.setContentProvider(new TimeZoneContentProvider());
+        treeViewer.setInput(new Object[] {TimeZoneComparator.getTimeZones()});
     }
 
+    @Focus
+    public void focus() {
+        treeViewer.getControl().setFocus();
+    }
 }
